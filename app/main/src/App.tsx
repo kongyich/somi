2006d4121b2b38547eb5695536f4efbe26fa6d80
kongyich <570588655@qq.com>
2024-02-22 21:19:15 +0800

    feat: add layout&router

--- app/main/src/App.tsx
+++ app/main/src/App.tsx
@@ -1,80 +1,9 @@
-import React, { useState } from 'react';
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu, Button, theme } from 'antd';
-import ListTable from './components/indexTable'
+import { FC } from 'react'
+import { RouterProvider } from 'react-router-dom'
+import routerConfig from './router'
 
-const { Header, Sider, Content } = Layout;
+const App: FC = () => {
+  return <RouterProvider router={routerConfig}></RouterProvider>
+}
 
-const App: React.FC = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
-        <Content
-          style={{
-            width: '100%'
-            // margin: '24px 16px',
-            // padding: 24,
-            // minHeight: 280,
-            // background: colorBgContainer,
-            // borderRadius: borderRadiusLG,
-          }}
-        >
-          <div style={{ paddingLeft: '40px' }}>
-            <ListTable />
-          </div>
-
-        </Content>
-      </Layout>
-    </Layout>
-  );
-};
-
-export default App;
+export default App
