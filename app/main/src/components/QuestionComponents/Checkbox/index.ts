4350142cbbcacefe86b228396caac29c8ec48c8e
kongyich <570588655@qq.com>
2024-03-26 21:49:01 +0800

    feat: add Checkbox component

---
+++ app/main/src/components/QuestionComponents/Checkbox/index.ts
@@ -0,0 +1,12 @@
+import Component from "./Component";
+import PropComponent from "./PropComponent";
+import { QuestionCheckboxDefaultProps } from "./interface";
+
+
+export default {
+  title: "多选",
+  type: "QuestionCheckbox",
+  Component,
+  PropComponent,
+  defaultProps: QuestionCheckboxDefaultProps
+}
