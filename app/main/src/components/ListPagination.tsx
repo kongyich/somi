b931b19bfa601097501deca5b95e710dff2f4f63
kongyich <570588655@qq.com>
2024-03-30 10:44:30 +0800

    feat: add star list

---
+++ app/main/src/components/ListPagination.tsx
@@ -0,0 +1,40 @@
+import { FC, useEffect, useState } from 'react'
+import { Pagination } from 'antd'
+import { LIST_PAGE_SIZE, LIST_PAGE_PARAM_KEY, LIST_PAGE_SIZE_PARAM_KEY } from '../constant'
+import { useSearchParams, useLocation, useNavigate } from 'react-router-dom'
+
+type PropsType = {
+  total: number
+}
+
+const ListPagination: FC<PropsType> = (props) => {
+  const { total } = props
+  const [searchParams] = useSearchParams()
+  const nav = useNavigate()
+  const { pathname } = useLocation()
+
+  const [current, setCurrent] = useState(1)
+  const [pageSize, setPageSize] = useState(LIST_PAGE_SIZE)
+
+  useEffect(() => {
+    const page = parseInt(searchParams.get(LIST_PAGE_PARAM_KEY) || '') || 1
+    setCurrent(page)
+    const pageSize = parseInt(searchParams.get(LIST_PAGE_SIZE_PARAM_KEY) || '') || LIST_PAGE_SIZE
+    setPageSize(pageSize)
+  }, [searchParams])
+
+  function handlePageChange(page: number, pageSize: number) {
+    searchParams.set(LIST_PAGE_PARAM_KEY, page.toString())
+    searchParams.set(LIST_PAGE_SIZE_PARAM_KEY, pageSize.toString())
+
+    nav({
+      pathname,
+      search: searchParams.toString()
+    })
+  }
+  return (
+    <Pagination current={current} pageSize={pageSize} total={total} onChange={handlePageChange} />
+  )
+}
+
+export default ListPagination
--- app/main/src/pages/Manage/Star.tsx
+++ app/main/src/pages/Manage/Star.tsx
@@ -1,8 +1,55 @@
 import { FC } from 'react'
+import styles from './styles/common.module.scss'
+import { Typography, Empty, Spin } from 'antd'
+import ListSearch from '../../components/ListSearch'
+import QuestionCard from '../../components/QuestionCard'
+import ListPagination from '../../components/ListPagination'
 
+const { Title } = Typography
 const Star: FC = () => {
+
+  const loading = false
+  const total = 10
+  const list = [
+    {
+      _id: '12123',
+      title: 'test2',
+      isStar: true,
+      isPublished: true,
+      answerCount: 23,
+      createdAt: '2024-12-22'
+    }
+  ]
+
   return (
-    <div>Star</div>
+    <>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <Title level={3}>star questions</Title>
+        </div>
+        <div className={styles.right}>
+          <ListSearch />
+        </div>
+      </div>
+      <div className={styles.content}>
+        {loading && (
+          <div style={{ textAlign: 'center' }}>
+            <Spin />
+          </div>
+        )}
+        {list.length === 0 && <Empty description="暂无数据" />}
+        {
+          list.length > 0 &&
+          list.map(q => {
+            const { _id } = q
+            return <QuestionCard key={_id} {...q} />
+          })
+        }
+      </div>
+      <div className={styles.footer}>
+        <ListPagination total={total} />
+      </div>
+    </>
   )
 }
 
